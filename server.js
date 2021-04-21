595fb460cacf64d44ac670e307ca28f6b3a200c0
SzarneckiP <siarniuk@gmail.com>
2021-04-21 13:08:33 +0200

    Add routes

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/', testimonialsRoutes);
-app.use('/', concertsRoutes);
-app.use('/', seatsRoutes);
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
 
 
 app.use((req, res) => {
