5c6842ab685542a58f039d2e64352ecc51e2c895
SzarneckiP <siarniuk@gmail.com>
2021-04-22 18:33:54 +0200

    fix all routes

---
+++ client/build/precache-manifest.aecf366e2e5e8c83608ed55c48e4cb3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a43ee67ffe952a227cb4bc84d3dc0eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef37d29dbeecd503e200",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "e613fede44c9df4dada6",
+    "url": "/static/css/main.25cbfd9d.chunk.css"
+  },
+  {
+    "revision": "ef37d29dbeecd503e200",
+    "url": "/static/js/2.339f3c1a.chunk.js"
+  },
+  {
+    "revision": "e613fede44c9df4dada6",
+    "url": "/static/js/main.134ea0a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
