e17840d885cccfe65644a5b95d76156d34d1a7db
SzarneckiP <siarniuk@gmail.com>
2021-05-01 20:52:01 +0200

    heroku init

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 const { v4: uuidv4 } = require('uuid');
-const { seats } = require('../db');
 
 
 router.route('/seats').get((req, res) => {
